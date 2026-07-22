79c55d6814bd61440ba8ee81ab420821b2ed0885
manasraghu <rmanas971@gmail.com>
2026-07-22 22:24:28 +0800

    Add files via upload
    initial commit

---
+++ pics/home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title = "home",
+    page_icon = ":woman_getting_massage:",
+    layout = "wide")
+
+st.title("home")
+st.sidebar.success("select a page above")
+
